ae53b177c2af69d3b90aa06e64f6c2ef4791e22b
Andrés López <andreslp08@hotmail.com>
2022-02-15 13:58:25 -0500

    se agrego un reducer para actualizar un instituto

--- src/redux/slices/institutions.silce.tsx
+++ src/redux/slices/institutions.silce.tsx
@@ -5,9 +5,17 @@ export const institutionsSlice = createSlice({
 	initialState: [] as Institution[],
 	name: "institutions",
 	reducers: {
-		set:(state, action:PayloadAction<Institution[]>)=>{
+		set: (state, action: PayloadAction<Institution[]>) => {
 			return action.payload;
-		}
+		},
+		updateOne: (state, action: PayloadAction<{id:string, institution:Institution | undefined}>) => {
+			if(action.payload.institution){
+				const filtered = state.filter(inst=>inst.id != action.payload.id);
+				return [...filtered, action.payload.institution];
+			}else{
+				return state;
+			}
+		},
 		// add: (state, action: PayloadAction<Todo>) => {
 		// 	state.push(action.payload);
 		// },
